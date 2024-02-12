a6eeb1bc5a4c6865278ade80c9be1b8a37ea5972
montexbjeliseo <secondmtx@gmail.com>
2024-02-12 12:13:04 -0300

    "Backend project initialization"

---
+++ backend/src/main/java/com/nocountry/c1634mjava/petsbackend/PetsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nocountry.c1634mjava.petsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetsBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/nocountry/c1634mjava/petsbackend/PetsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nocountry.c1634mjava.petsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
