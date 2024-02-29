f035b31add8c7ff1bf367204bde597c95f25d8df
Jesica <jesicamachetti@Air-de-Jesica.fibertel.com.ar>
2024-02-29 10:40:12 -0300

    agregue secciones registro ingresar

---
+++ frontend/app/registrarse/page.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Formularioregistro from "@/components/formularioregistro";
+import Footer from "@/components/footer";
+
+
+
+const page = () => {
+  return (
+    <div>
+      <Formularioregistro/>
+      <Footer/>
+
+    </div>
+  );
+};
+
+export default page;
--- frontend/components/header.jsx
+++ frontend/components/header.jsx
@@ -25,11 +25,12 @@ export default function Header() {
           <Link href="/contactanos" className={stlye.navButton}>
             Contactanos
           </Link>
+          <Link href="/registrarse" className={stlye.registerButton}>Registrarse
+          </Link>
+          <Link href="/ingreso" className={stlye.loginButton}>Ingresar
+          </Link>
         </nav>
-        <div className={stlye.loginContainer}>
-          <button className={stlye.registerButton}>Registrarse</button>
-          <button className={stlye.loginButton}>Ingresar</button>
-        </div>
+        
       </div>
     </header>
   );
