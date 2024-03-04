ff8bd8ee7e4fb0f2e2f41b66f9766a30d94d91fe
montexbjeliseo <secondmtx@gmail.com>
2024-03-04 12:55:00 -0300

    Pequeño ajuste en el navbar

--- frontend/components/header.jsx
+++ frontend/components/header.jsx
@@ -10,27 +10,15 @@ export default function Header() {
       <div className={stlye.navBarContainer}>
         <Image src={logo} width="300" height="40" alt="imagen logotipo" />
         <nav className={stlye.navButtonContainer}>
-          <Link href="/" className={stlye.navButton}>
-            Inicio
-          </Link>
-
-          <Link href="/nosotros" className={stlye.navButton}>
-            Sobre Nosotros
-          </Link>
-
-          <Link href="/adopcion" className={stlye.navButton}>
-            Adoptame
-          </Link>
-
-          <Link href="/contactanos" className={stlye.navButton}>
-            Contactanos
-          </Link>
-          <Link href="/registrarse" className={stlye.registerButton}>Registrarse
-          </Link>
-          <Link href="/ingreso" className={stlye.loginButton}>Ingresar
-          </Link>
+          <Link href="/" className={stlye.navButton}>Inicio</Link>
+          <Link href="/nosotros" className={stlye.navButton}>Sobre Nosotros</Link>
+          <Link href="/adopcion" className={stlye.navButton}>Adoptame</Link>
+          <Link href="/contactanos" className={stlye.navButton}>Contactanos</Link>
         </nav>
-
+        <nav>
+          <Link href="/registrarse" className={stlye.registerButton}>Registrarse</Link>
+          <Link href="/ingreso" className={stlye.loginButton}>Ingresar</Link>
+        </nav>
       </div>
     </header>
   );
