dbcde784fe8bf94268e4788a058230edb22d46f2
montexbjeliseo <secondmtx@gmail.com>
2024-03-07 01:15:24 -0300

    Fetch forms

--- frontend/components/panels/PetPanel.jsx
+++ frontend/components/panels/PetPanel.jsx
@@ -1,18 +1,64 @@
 import { PetIcon } from "@/icons/PetIcon"
 import { PanelLayout } from "./PanelLayout"
 
+import style from "./PetPanel.module.css";
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { fetchPets } from "@/services/actions/fetch-pets";
+
+const PetPanelElement = ({ name, age, gender, breed, city, image, ...props }) => {
+    return (
+        <div className={style.element}>
+            <div>{name}</div>
+            <div>{age}</div>
+            <div>{gender}</div>
+            <div>{breed}</div>
+            <div>{city}</div>
+            <div>{image}</div>
+        </div>
+    )
+}
+
 export const PetPanel = () => {
+
+    const [loading, setLoading] = useState(true);
+
+    const [error, setError] = useState(null);
+
+    const [data, setData] = useState(null);
+
+    const token = useSelector((state) => state.auth.token);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetchPets({offset: 0, limit: 50});
+                if (response) {
+                    setData(response);
+                    setLoading(false);
+                }
+            } catch (error) {
+                setError('Comprueba tu conexión a internet');
+                setLoading(false);
+            }
+        }
+
+        if (token) {
+            fetchData();
+        }
+    }, [token]);
+
     return (
         <div>
-            <h2>Panel de solicitud de mascotas</h2>
+            <h2>Panel de mascotas</h2>
             <PanelLayout title="Panel de mascotas" icon={<PetIcon color="#ff0000" />}>
-                <ul>
-                    <li>Elemento 1</li>
-                    <li>Elemento 1</li>
-                    <li>Elemento 1</li>
-                    <li>Elemento 1</li>
-                    <li>Elemento 1</li>
-                </ul>
+                {data && (
+                    <div className={style.container}>
+                        {data.map((pet) => <PetPanelElement key={pet.id} {...pet} />)}
+                    </div>
+                )}
+                {error && <div>{error}</div>}
+                {loading && <div>Cargando...</div>}
             </PanelLayout>
         </div>
     )
--- frontend/components/panels/RequestPanel.jsx
+++ frontend/components/panels/RequestPanel.jsx
@@ -1,18 +1,71 @@
 import { BoneIcon } from "@/icons/BoneIcon"
 import { PanelLayout } from "./PanelLayout"
+import style from "./RequestPanel.module.css";
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { fetchForms } from "@/services/actions/fetch-forms";
+
+const RequestElement = ({ firstname, lastname, email }) => {
+    return (
+        <div className={style.element}>
+            <div>{firstname}</div>
+            <div>{lastname}</div>
+            <div>{email}</div>
+        </div>
+    )
+}
 
 export const RequestPanel = () => {
+
+    const [data, setData] = useState(null);
+
+    const [error, setError] = useState(null);
+
+    const [loading, setLoading] = useState(true);
+
+    const token = useSelector((state) => state.auth.token);
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetchForms(token);
+
+                if (response.status === 200) {
+                    const jsonData = await response.json();
+                    setData(jsonData);
+                    setLoading(false);
+                    return;
+                }
+
+                if (response.status === 204) {
+                    setError('No hay solicitudes');
+                    setLoading(false);
+                    return;
+                }
+
+                setError('Ocurrio un error mientras se cargaban las solicitudes');
+                setLoading(false);
+
+            } catch (error) {
+                setError('Comprueba tu conexión a internet');
+                setLoading(false);
+            }
+        }
+
+        if (token) {
+            fetchData();
+        }
+
+    }, [token]);
+
     return (
         <div>
             <h2>Panel de solicitudes</h2>
             <PanelLayout title="Solicitudes de adopción" icon={<BoneIcon color="#ff0000" />}>
-                <ul>
-                    <li>Elemento 1</li>
-                    <li>Elemento 1</li>
-                    <li>Elemento 1</li>
-                    <li>Elemento 1</li>
-                    <li>Elemento 1</li>
-                </ul>
+                {error && <div>{error}</div>}
+                {loading && <div>Cargando...</div>}
+                {data && data.map((form) => <RequestElement key={form.id} {...form} />)}
             </PanelLayout>
         </div>
     )
